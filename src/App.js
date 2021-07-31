a6ff55db2b9b759352568e61623833da894b3c46
Haneenabonser <aabonser@gmail.com>
2021-07-31 19:54:27 +0300

    signup-done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Signup from './components/Signup';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/">
+          <Signup />
+          </Route>
+          <Route path="/signin">
+          </Route>
+          <Route path="/signup">
+          </Route>
+          <Route path="/AboutUs">
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    );
+  }
 }
 
 export default App;
