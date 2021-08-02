b5430312929c0476c2044d6a9afc15d3145bb380
sukina12 <sukina_89@yahoo.com>
2021-08-02 11:41:03 +0300

    done chat socket

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -5,6 +5,7 @@ import base64 from 'base-64';
 // import { Redirect } from 'react-router-dom';
 import MyCourses from './MyCourses';
 import AllCourses from './AllCourses';
+import Chat from './Chat';
 
 export class Signin extends Component {
 
@@ -13,9 +14,11 @@ export class Signin extends Component {
     this.state = {
       email: '',
       password: '',
-      showStudent: false,
-      showTeacher: false,
-      showForm: true
+      NickName:'',
+      // showStudent: false,
+      // showTeacher: false,
+      showForm: true,
+      showChat:false,
     }
   }
 
@@ -23,53 +26,62 @@ export class Signin extends Component {
     this.setState({ [e.target.name]: e.target.value });
   };
 
-  studentSignIn = async (e) => {
+  studentSignIn = (e) => {
     e.preventDefault();
+    this.setState({
+      showChat : true,
+    })
+  }
 
-    const base1 = base64.encode(`${this.state.email}:${this.state.password}`);
-    console.log('base1', base1);
-    const basic = `Basic ${base1}`;
+  // studentSignIn = async (e) => {
+  //   e.preventDefault();
+
+  //   const base1 = base64.encode(`${this.state.email}:${this.state.password}`);
+  //   console.log('base1', base1);
+  //   const basic = `Basic ${base1}`;
 
 
-    const payload = {
-      headers: { authorization: basic },
-    }
-    console.log(payload);
-    const allData = await axios.get('http://localhost:3001/getUsers');
-    console.log('all data', allData);
+  //   const payload = {
+  //     headers: { authorization: basic },
+  //   }
+  //   console.log(payload);
+  //   const allData = await axios.get('http://localhost:3001/getUsers');
+  //   console.log('all data', allData);
 
-    const user = allData.data.filter((user, idx) => {
+  //   const user = allData.data.filter((user, idx) => {
 
-      return (user.email === this.state.email);
-    });
-    console.log('user', user[0].role);
-    if (user[0]) {
+  //     return (user.email === this.state.email);
+  //   });
+  //   console.log('user', user[0].role);
+  //   if (user[0]) {
 
-      if (user[0].role === 'user') {
-        const student = await axios.post('http://localhost:3001/signin/user', payload);
-        alert(student.data);
-        if (student.data === 'you are signedIn!') {
-          this.setState({ showStudent: true, showForm: false })
-        }
-      } else if (user[0].role === 'admin') {
+  //     if (user[0].role === 'user') {
+  //       const student = await axios.post('http://localhost:3001/signin/user', payload);
+  //       alert(student.data);
+  //       if (student.data === 'you are signedIn!') {
+  //         this.setState({ showStudent: true, showForm: false })
+  //       }
+  //     } else if (user[0].role === 'admin') {
 
-        const teacher = await axios.post('http://localhost:3001/signin/teacher', payload);
-        alert(teacher.data);
-        // console.log('rolee',user[0].role);
-        if (teacher.data === 'you are signedIn!') {
-          this.setState({ showTeacher: true, showForm: false })
-        }
-      }
-    } else {
-      alert('You are not signedUp !')
-    }
+  //       const teacher = await axios.post('http://localhost:3001/signin/teacher', payload);
+  //       alert(teacher.data);
+  //       // console.log('rolee',user[0].role);
+  //       if (teacher.data === 'you are signedIn!') {
+  //         this.setState({ showTeacher: true, showForm: false })
+  //       }
+  //     }
+  //   } else {
+  //     alert('You are not signedUp !')
+  //   }
 
-    // const newStudent = await axios.post('http://localhost:3001/signup', payload);
-    // alert(newStudent.data);
+  //   // const newStudent = await axios.post('http://localhost:3001/signup', payload);
+  //   // alert(newStudent.data);
 
-    //     socket.emit('signup', payload);
-    //     console.log(payload);
-  };
+  //   //     socket.emit('signup', payload);
+  //   //     console.log(payload);
+  // };
+
+
 
 
 
@@ -77,14 +89,19 @@ export class Signin extends Component {
 
     return (
       <>
-        {this.state.showTeacher && <MyCourses />}
-        {this.state.showStudent && <AllCourses />}
-
+        {/* {this.state.showTeacher && <MyCourses />} */}
+        {/* {this.state.showTeacher && <Chat />} */}
+        {/* {this.state.showStudent && <AllCourses />} */}
+        {this.state.showChat && <Chat NickName={this.state.NickName}/>}
         {this.state.showForm &&
           <Form onSubmit={this.studentSignIn}>
             <Form.Group className="mb-3">
               <Form.Label>Email address</Form.Label>
               <Form.Control type="email" placeholder="Enter email" name="email" onChange={this.handleChange} />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Nick Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter NickName" name="NickName" onChange={this.handleChange} />
             </Form.Group>
 
             <Form.Group className="mb-3">
@@ -97,6 +114,7 @@ export class Signin extends Component {
             </Button>
           </Form>
         }
+        
       </>
     )
   }
